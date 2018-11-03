d88a92e89bd0723e98d3df55f57175d7b8f0bdb6
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-11-03 12:53:15 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

--- m6/ADS-2Week-1/Solution.java
+++ m6/ADS-2Week-1/Solution.java
@@ -1,11 +1,25 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class PageRank {	
+/**
+ * Class for page rank.
+ */
+class PageRank {
 }
+/**
+ * Class for web search.
+ */
 class WebSearch {
 }
-public class Solution {
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+class Solution {
+	/**
+	 * { function_description }.
+	 *
+	 * @param      args  The arguments.
+	 */
+	public static void main(final String[] args) {
 	     String file = "WebContent.txt";
 	}
 }
