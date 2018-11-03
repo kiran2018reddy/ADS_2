48813fa19fd86af4eec0b0006e0b3ced40426760
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-11-03 12:51:24 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

--- m6/ADS-2Week-1/Solution.java
+++ m6/ADS-2Week-1/Solution.java
@@ -1,50 +1,11 @@
 import java.util.Scanner;
 import java.util.Arrays;
-class PageRank {
-	
-	
+class PageRank {	
 }
-
 class WebSearch {
-
 }
-
-
 public class Solution {
 	public static void main(String[] args) {
-	      // int nodes,i,j,cost;
-       //  Scanner in = new Scanner(System.in);
-       //  System.out.println("Enter the Number of WebPages \n");
-       //  nodes = in.nextInt();
-       //  PageRank p = new PageRank();
-       //  System.out.println("Enter the Adjacency Matrix with 1->PATH & 0->NO PATH Between two WebPages: \n");
-       //  for(i=1;i<=nodes;i++)
-       //    for(j=1;j<=nodes;j++);
-        // p.calc(nodes);
-
-		// read the first line of the input to get the number of vertices
-
-		// iterate count of vertices times 
-		// to read the adjacency list from std input
-		// and build the graph
-		
-		
-		// Create page rank object and pass the graph object to the constructor
-		
-		// print the page rank object
-		
-		// This part is only for the final test case
-		
-		// File path to the web content
-		String file = "WebContent.txt";
-		
-		// instantiate web search object
-		// and pass the page rank object and the file path to the constructor
-		
-		// read the search queries from std in
-		// remove the q= prefix and extract the search word
-		// pass the word to iAmFeelingLucky method of web search
-		// print the return value of iAmFeelingLucky
-		
+	     String file = "WebContent.txt";
 	}
 }
