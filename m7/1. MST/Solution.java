55bee3f1e413f635f20811a16febaa0eebada337
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-11-05 21:02:39 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- m7/1. MST/Solution.java
+++ m7/1. MST/Solution.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int vertices = Integer.parseInt(input.nextLine());
-		int edges = Integer.parseInt(input.nextLine());
-		EdgeWeightedGraph edgeW = new EdgeWeightedGraph(vertices);
-		for(int i = 0; i < edges; i++) {
-		String[] tokens = input.nextLine().split(" ");
-		Edge edge = new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2]));
-		edgeW.addEdge(edge);
-
-		}
-		KruskalMST kmst = new KruskalMST(edgeW);
-		System.out.format("%.5f", kmst.weight());
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+        int vertices = Integer.parseInt(input.nextLine());
+        int edges = Integer.parseInt(input.nextLine());
+        EdgeWeightedGraph edgeW = new EdgeWeightedGraph(vertices);
+        for (int i = 0; i < edges; i++) {
+        String[] tokens = input.nextLine().split(" ");
+Edge edge = new Edge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]),
+Double.parseDouble(tokens[2]));
+        edgeW.addEdge(edge);
+        }
+        KruskalMST kmst = new KruskalMST(edgeW);
+        System.out.format("%.5f", kmst.weight());
+    }
 }
