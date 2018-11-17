2764629d639da8164072947163ee945cd84f24d7
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-11-17 12:48:26 +0530

    "check_style_score -> 3 of 5 passed. style score: 0/1 "

--- m18/ADS-2Exam-3/Solution.java
+++ m18/ADS-2Exam-3/Solution.java
@@ -172,7 +172,7 @@ class T9 {
      * @return     The suggestions.
      */
 public final Iterable<String>
-getSuggestions(final Iterable<String> words,final int k) {
+getSuggestions(final Iterable<String> words, final int k) {
         // your code goes here
         ArrayList<String> arr = new ArrayList<>();
         MaxPQ<Integer> max = new MaxPQ<>();
@@ -199,7 +199,7 @@ getSuggestions(final Iterable<String> words,final int k) {
      *
      * @return     { description_of_the_return_value }.
      */
-    public final  Iterable<String> t9(final String t9Signature,final int k) {
+    public final  Iterable<String> t9(final String t9Signature, final int k) {
         return getSuggestions(potentialWords(t9Signature), k);
     }
 }
